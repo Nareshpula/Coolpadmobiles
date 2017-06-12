38f05803e820224fe54e54f542aed06f80851d3d
Naresh <Nareshroyal@gmail.com>
2017-06-12 02:17:52 -0700

    i'm adding something in future file

--- future.java
+++ future.java
@@ -1,4 +1,5 @@
 this is the created by the file called they are created by the file.
 may be action they can easy to share and point io the future file.
 may take action this situation. my jobs are go away sir.
+i'm adding something about value called A=520(this is the good value).
 so this time may be capture for this file.
