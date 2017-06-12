a9f8925a231f0cea59e2b340cab4cbf7a8e0d21e
Naresh <Nareshroyal@gmail.com>
2017-06-12 02:19:46 -0700

    i commited only the modified value

--- capital.java
+++ capital.java
@@ -1,3 +1,4 @@
 this the file called capital.
 they can easy to acceptable for the file.it has to pay the operations
+i am adding the value Q=123.
 that's what to pay the operations and i getting to file.
